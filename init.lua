7683460401891b16d6cb19e108dea41535addfc8
chris <cochranschris@gmail.com>
2025-06-21 13:24:17 -0500

    custom md lsp refactor

--- init.lua
+++ init.lua
@@ -67,28 +67,7 @@ require('lazy').setup({
     },
 })
 
-vim.lsp.set_log_level 'debug'
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'markdown',
-    callback = function()
-        local client = vim.lsp.start {
-            name = 'python-lsp-chris',
-            cmd = { '/home/chris/Development/nvim/python-lsp/dist/main' },
-            root_dir = '/home/chris/Development/nvim/python-lsp/',
-            on_error = function(code, _)
-                vim.notify(vim.lsp.client_errors[code], vim.log.levels.WARN)
-            end,
-            trace = 'verbose',
-        }
-
-        if not client then
-            vim.notify "Hey, you didn't do the client thing good"
-            return
-        end
-
-        local output = vim.lsp.buf_attach_client(0, client)
-        vim.notify('client attach success: ' .. tostring(output), vim.log.levels.INFO)
-    end,
-})
+-- connect to custom lsp
+require 'cc.custom.python-lsp-chris'
 
 -- vim: ts=2 sts=2 sw=2 et
